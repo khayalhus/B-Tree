f43f81f2f51bcb78de49de7edabe1f4c21b167cc
Khayal H <43758778+khmaxim@users.noreply.github.com>
2021-02-03 15:54:09 +0400

    Implement insertion and fix print
    Fix printing logic to be preorder
    Implement insert, insertNonFull, and split

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ struct Node {
     Node(int t, bool leaf); // constructor
     ~Node();
 
-    void insert(int x, int y, char z, char key);    // insert to node
-    void split(int index, Node* y); // split node
+    void insertNonFull(int x, int y, char z, char key);    // insert to node
+    void split(int index, Node* y); // y is the child of this node with order index and (y) is the node being split
     void print();   // print node
 };
 
@@ -25,9 +25,10 @@ Node::Node(int t, bool leaf) {
     this->y = new int[2 * t - 1];   // key amount
     this->z = new char[2 * t - 1];  // key amount
 
-    this->childs = new Node * [2 * size];   // child amount
+    this->childs = new Node * [2 * t];   // child amount
     this->size = 0; // child size at start
     this->leaf = leaf;  // true if it is a leaf node, false otherwise
+    this->t = t;
 }
 
 Node::~Node() {
@@ -38,13 +39,17 @@ void Node::print() {
     // prints node in prefix order
     int index;
     for (index = 0; index < size; index++) {
+        printf("(%d,%d,%c)", x[index], y[index], z[index]);
+    }
+    printf("\n");
+    for (index = 0; index < size; index++) {
         if (leaf == false) {
             childs[index]->print();
         }
-        cout << "(" << x[index] << "," << y[index] << "," << z[index] << ")";
     }
-    if (leaf == false)
+    if (leaf == false) {
         childs[index]->print();
+    }
 }
 
 struct BTree {
@@ -77,16 +82,108 @@ void BTree::print() {
 
 void BTree::insert(int x, int y, char z) {
     // insert new key to B-Tree
+    Node *r = this->root;
+
+    if (r == NULL) {
+        // if root is undefined, create root and insert key
+        this->root = new Node(t, true);
+        this->root->x[0] = x;
+        this->root->y[0] = y;
+        this->root->z[0] = z;
+        this->root->size = 1;
+    } else {
+        if (root->size == 2 * t - 1) {
+            Node *s = new Node(t, false); // allocate node
+
+            this->root = s; // make new root
+
+            s->childs[0] = r;
+
+            s->split(0, r);
+
+            int i = 0;
+            if (this->key == 'x' && s->x[0] < x || this->key == 'y' && s->y[0] < y || this->key == 'z' && s->z[0] < z) {
+                i++;
+            }
+
+            s->childs[i]->insertNonFull(x, y, z, this->key);
+
+        } else {
+            r->insertNonFull(x, y, z, this->key);
+        }
+    }
 }
 
 
-void Node::insert(int x, int y, char z, char key) {
+void Node::insertNonFull(int x, int y, char z, char key) {
     // insert if it is not full
+    int i = this->size - 1;
+
+    if (this->leaf == true) {
+        while (i >= 0 && key == 'x' && this->x[i] > x || i >= 0 && key == 'y' && this->y[i] > y || i >= 0 && key == 'z' && this->z[i] > z)
+        {
+            this->x[i + 1] = this->x[i];
+            this->y[i + 1] = this->y[i];
+            this->z[i + 1] = this->z[i];
+            i = i - 1;
+        }
+
+        this->x[i + 1] = x;
+        this->y[i + 1] = y;
+        this->z[i + 1] = z;
+        this->size++;
+    } else {
+        while (i >= 0 && key == 'x' && this->x[i] > x || i >= 0 && key == 'y' && this->y[i] > y || i >= 0 && key == 'z' && this->z[i] > z) {
+            i--;
+        }
+
+        if (this->childs[i + 1]->size == 2 * t - 1) {
+
+            split(i + 1, this->childs[i + 1]);
+
+            if (key == 'x' && this->x[i + 1] < x || key == 'y' && this->y[i + 1] < y || key == 'z' && this->z[i+1] < z) {
+                i++;
+            }
+        }
+        this->childs[i + 1]->insertNonFull(x, y, z, key);
+    }
 }
 
 
-void Node::split(int i, Node* new_node) {
+void Node::split(int i, Node* y) {
     // split the child
+    Node *z = new Node(y->t, y->leaf);
+    z->size = this->t - 1;
+
+    for (int j = 0; j < t - 1; j++) {
+        z->x[j] = y->x[j + t];
+        z->y[j] = y->y[j + t];
+        z->z[j] = y->z[j + t];
+    }
+
+    if (y->leaf != true) {
+        for (int j = 0; j < t; j++) {
+            z->childs[j] = y->childs[j + t];
+        }
+    }
+
+    y->size = t - 1;
+    for (int j = this->size; j >= i + 1; j--) {
+        this->childs[j + 1] = this->childs[j];
+    }
+
+    this->childs[i + 1] = z;
+
+    for (int j = this->size - 1; j >= i; j--) {
+        this->x[j + 1] = this->x[j];
+        this->y[j + 1] = this->y[j];
+        this->z[j + 1] = this->z[j];
+    }
+
+    this->x[i] = y->x[this->t - 1];
+    this->y[i] = y->y[this->t - 1];
+    this->z[i] = y->z[this->t - 1];
+    this->size++;
 }
 
 int main() {
@@ -95,7 +192,6 @@ int main() {
     scanf("%d", &new_tree->node_count);
     scanf("%d", &new_tree->t);
     scanf(" %c", &new_tree->key);
-    printf("!%d!%d!%c!\n", new_tree->node_count, new_tree->t, new_tree->key);
 
     int x, y;
     char z;
@@ -103,11 +199,10 @@ int main() {
         scanf("%d", &x);
         scanf("%d", &y);
         scanf(" %c", &z);
-        printf("!%d!%d!%c!\n", x, y, z);
-        //new_tree->insert(x, y, z);
+        new_tree->insert(x, y, z);
     }
 
-    //new_tree->print();
+    new_tree->print();
 
     delete new_tree;
 
